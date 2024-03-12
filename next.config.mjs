f458d2687670899aa1f9e0ea13f593a6609eab89
YungChun Tu <ken888686@gmail.com>
2024-03-12 12:08:17 +0900

    update next config and github action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  assetPrefix: "https://ken888686.github.io/aaron-tu/",
+};
 
 export default nextConfig;
